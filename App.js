26629205f27914056db8401bd401b35fb7a38271
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-07-31 07:24:18 -0300

    Get the DB value | Compare 2 values

--- App.js
+++ App.js
@@ -5,17 +5,19 @@ require('dotenv').config();
 (async () => {
     // link Website
     const linkKabum = 'https://www.kabum.com.br/login?redirect_uri=https://www.kabum.com.br/minha-conta/favoritos'
+    const email = process.env.USER_EMAIL
+    const password = process.env.USER_PASSWORD
 
     // default for puppeteer
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
 
     // go to website
     await page.goto(linkKabum)
 
     // login
-    await page.type('.sc-iGgWBj.cnXDzr.inputForm input[type="text"]', process.env.USER_EMAIL)
-    await page.type('.sc-iGgWBj.cnXDzr.inputForm input[type="password"]', process.env.USER_PASSWORD)
+    await page.type('.sc-iGgWBj.cnXDzr.inputForm input[type="text"]', email)
+    await page.type('.sc-iGgWBj.cnXDzr.inputForm input[type="password"]', password)
     await page.keyboard.press('Tab')
     await page.keyboard.press('Enter')
 
@@ -45,8 +47,16 @@ require('dotenv').config();
         // getting the all value of fav products price
         const price = await frame.$$('xpath/' + '//div[@class="sc-eeDRCY cJyymn productPrice"]/h1')
 
-        // initiating the product array
-        let readyProducts = []
+        async function getDataFromJSONServer(id) {
+            try {
+                const response = await axios.get(`http://localhost:5000/products/${id + 1}`);
+                let dado = response.data;
+                return dado
+
+            } catch (error) {
+                console.error('Erro ao obter os dados do JSON-Server(GET):', error);
+            }
+        }
 
         // function to send products to db.json
         async function sendDataToJSONServer(data) {
@@ -59,23 +69,42 @@ require('dotenv').config();
             }
         }
 
+
+        const productDb = await getDataFromJSONServer()
+
+        // initiating the product array
+        let readyProduct = []
+
         // loop over the quantity of products
         for (let i = 0; i < title.length; i++) {
+
+            const productDb = await getDataFromJSONServer(i)
+            // getDataFromJSONServer()
             // getting the value of h1 in product title div
             let productTitle = await title[i].evaluate(el => el.innerText);
             // transforming the data
-            productTitle = (productTitle.slice(0, 30)).replace(',','')
+            productTitle = (productTitle.slice(0, 30)).replace(',', '')
             // getting the value of h1 in product price div
             let productPrice = await price[i].evaluate(el => el.innerText)
             // transforming the data
             productPrice = parseFloat((productPrice.slice(3, 10)).replace(',', '.'))
-
             // organizing the data to send to db.json
-            readyProducts = { id: i + 1, title: productTitle, price: productPrice }
+            readyProduct = { id: i + 1, title: productTitle, price: productPrice }
 
-            sendDataToJSONServer(readyProducts);
+            if (productPrice < productDb.price) {
+                // sendDataToJSONServer(readyProduct);
+                console.log(`O produto ${productDb.title} está R$ ${productDb.price - productPrice} mais barato.`)
+            } else {
+                console.log('Mesmos valores :(')
+                // console.log(readyProduct)
+                // console.log(dbPrice)
+            }
+
+
         }
 
-    }
 
+
+    }
+    await browser.close()
 })()
